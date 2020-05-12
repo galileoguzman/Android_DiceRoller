429be9f71723f4f77500ed42539e69a645d80d55
galileoguzman <galileoguzman@gmail.com>
2020-05-12 11:59:02 -0500

    Added images resource as result of random number

--- app/src/main/java/com/galileoguzman/diceroller/MainActivity.kt
+++ app/src/main/java/com/galileoguzman/diceroller/MainActivity.kt
@@ -10,13 +10,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // On click listener for roll_button
         roll_button.setOnClickListener{
+            // This segment happen every time the button is clicked
             rollDice()
         }
     }
 
+    // Private function
     private fun rollDice() {
+        // Default image before random calc is done
+        dice_image.setImageResource(R.drawable.empty_dice)
+
+        // Get random calc
         val randomNumber = Random.nextInt(6) + 1
-        result_text.text = randomNumber.toString()
+
+        // Switch case in kotlin
+        val drawableResource = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Setting up random dice
+        dice_image.setImageResource(drawableResource)
     }
 }
